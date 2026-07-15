4e9a9e94a1eec5008d5252d0fc9138b925735488
Raymond Chu <arrraichu@gmail.com>
2026-07-15 05:23:02 -0400

    feat : pill markup

--- app/_components/content/simple-markup.tsx
+++ app/_components/content/simple-markup.tsx
@@ -7,6 +7,7 @@ export default function SimpleMarkup(text: string, className?: string): React.Re
   let k = 0;
   let remaining = text;
   let danglingTag = '';
+  let extras = '';
 
   while (true) {
     const match = RE_MARKUP_FORMAT.exec(remaining);
@@ -38,14 +39,15 @@ export default function SimpleMarkup(text: string, className?: string): React.Re
       }
 
       const formatText = remaining.slice(0, index);
-      components.push(processTag(danglingTag, formatText, k));
+      components.push(processTag(danglingTag, formatText, k, extras));
       k++;
       danglingTag = '';
+      extras = '';
 
       remaining = remaining.slice(index + tagLength);
 
       if (remaining.length <= 0) {
-        return components;
+        break;
       }
       continue;
     }
@@ -62,7 +64,11 @@ export default function SimpleMarkup(text: string, className?: string): React.Re
         <div key={k} className="inline">{remaining.slice(0, index)}</div>
       );
       k++;
-      danglingTag = tag;
+      const parts = tag.split(':');
+      danglingTag = parts[0];
+      if (parts.length > 1) {
+        extras = parts[1];
+      }
       remaining = remaining.slice(index + tagLength);
     }
   }
@@ -73,7 +79,7 @@ export default function SimpleMarkup(text: string, className?: string): React.Re
   </div>;
 }
 
-function processTag(tag: string, text: string, index: number): React.ReactNode {
+function processTag(tag: string, text: string, index: number, extras: string): React.ReactNode {
   switch (tag) {
     case 'b': {
       return <b key={index} className="inline px-1">
@@ -97,6 +103,12 @@ function processTag(tag: string, text: string, index: number): React.ReactNode {
         </span>
       </div>;
     }
+    case 'pill': {
+      return <div key={index} className={clsx(
+        'inline-block px-2 py-0.5 mr-2 rounded-2xl text-sm text-black dark:text-white border',
+        COLOR_CLASSES[extras] ?? COLOR_CLASSES.gray
+      )}>{text}</div>;
+    }
     case 'br': {
       return <div key={index} className="py-2"></div>;
     }
@@ -105,3 +117,16 @@ function processTag(tag: string, text: string, index: number): React.ReactNode {
     }
   }
 }
+
+const COLOR_CLASSES: Record<string, string> = {
+  /* DEFAULT */
+  gray: 'border-slate-700/50 bg-slate-300 dark:border-slate-300/50 dark:bg-slate-700',
+
+  /* USE THESE COLORS PRIMARILY */
+  red: 'border-red-700/50 bg-red-300 dark:border-red-300/50 dark:bg-red-700',
+  orange: 'border-orange-700/50 bg-orange-300 dark:border-orange-300/50 dark:bg-orange-700',
+  yellow: 'border-yellow-700/50 bg-yellow-300 dark:border-yellow-300/50 dark:bg-yellow-700',
+  green: 'border-green-700/50 bg-green-300 dark:border-green-300/50 dark:bg-green-700',
+  blue: 'border-blue-700/50 bg-blue-300 dark:border-blue-300/50 dark:bg-blue-700',
+  violet: 'border-violet-700/50 bg-violet-300 dark:border-violet-300/50 dark:bg-violet-700'
+};

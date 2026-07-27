592dfee449991f3e26ef819f4352c0924bbfeac3
Raymond Chu <arrraichu@gmail.com>
2026-07-27 03:41:06 -0400

    feat : footer content

---
+++ app/_components/content/footer-content.tsx
@@ -0,0 +1,9 @@
+export default function FooterContent() {
+  return (
+    <section className="flex justify-center mt-12 py-2 md:py-3 lg:py-4">
+      <a href="#" className="text-(--line-color) opacity-75 hover:opacity-100">
+        Back to top ^
+      </a>
+    </section>
+  );
+}
